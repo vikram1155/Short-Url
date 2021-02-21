b514ec4e7e482e001bd49c7de341976479b659e0
vikram <markiv1155@gmail.com>
2021-02-21 16:28:34 +0530

    Updates

---
+++ precache-manifest.3905a475684e4ecfffa12e7a1c60a0a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7e16839754cbdc30acef3f31ce8b63",
+    "url": "/Short-Url/index.html"
+  },
+  {
+    "revision": "bc2f64717e5ba3b17d9f",
+    "url": "/Short-Url/static/js/2.1f17c64a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Short-Url/static/js/2.1f17c64a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea35e8d6e848e4476a6c",
+    "url": "/Short-Url/static/js/main.b22070e8.chunk.js"
+  },
+  {
+    "revision": "b55197ea6138f7e90648",
+    "url": "/Short-Url/static/js/runtime-main.6d3271ea.js"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/Short-Url/static/media/illustration-working.5029d049.svg"
+  }
+]);
